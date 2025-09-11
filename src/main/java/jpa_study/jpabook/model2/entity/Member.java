907838d7ab1f84b81188c08689985edfdbbc85c3
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-11 17:17:59 +0900

    model 2 작성

---
+++ src/main/java/jpa_study/jpabook/model2/entity/Member.java
@@ -0,0 +1,35 @@
+package jpa_study.jpabook.model2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<Order>();
+}
+
---
+++ src/main/java/jpa_study/jpabook/model2/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package jpa_study.jpabook.model2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "ORDER_ITEM")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;      // 주문 상품
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;    // 주문
+
+    private int orderPrice; // 주문 가격
+    private int count;      // 주문 수량
+}
